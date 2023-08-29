d901a443c3bbcecfb5af05d20c09602542750d2e
Maciej Komorowski <maciej.komorowski@cloudinary.com>
2023-08-29 14:28:38 +0200

    Fixes paramters delimiter in BackgroundRemoval action

--- __TESTS__/unit/fromJson/backgroundRemoval.fromJson.test.ts
+++ __TESTS__/unit/fromJson/backgroundRemoval.fromJson.test.ts
@@ -1,24 +1,37 @@
-import {fromJson} from "../../../src/internal/fromJson";
-import {ForegroundObject} from "../../../src/qualifiers/foregroundObject";
+import { fromJson } from "../../../src/internal/fromJson";
+import { ForegroundObject } from "../../../src/qualifiers/foregroundObject";
 
-describe('backgroundRemoval.fromJson', () => {
-  it('should generate a url with backgroundRemoval', function () {
-    const transformation = fromJson({actions:[
-      { actionType: 'backgroundRemoval'},
-      { actionType: 'backgroundRemoval', fineEdges: true},
-      { actionType: 'backgroundRemoval', fineEdges: false},
-      { actionType: 'backgroundRemoval', hints: []},
-      { actionType: 'backgroundRemoval', hints: [ForegroundObject.DOG, ForegroundObject.CAT]},
-      { actionType: 'backgroundRemoval', fineEdges: true, hints: [ForegroundObject.DOG, ForegroundObject.CAT]},
-    ]});
+describe("backgroundRemoval.fromJson", () => {
+  it("should generate a url with backgroundRemoval", function () {
+    const transformation = fromJson({
+      actions: [
+        { actionType: "backgroundRemoval" },
+        { actionType: "backgroundRemoval", fineEdges: true },
+        { actionType: "backgroundRemoval", fineEdges: false },
+        { actionType: "backgroundRemoval", hints: [] },
+        { actionType: "backgroundRemoval", hints: [ForegroundObject.DOG] },
+        {
+          actionType: "backgroundRemoval",
+          hints: [ForegroundObject.DOG, ForegroundObject.CAT],
+        },
+        {
+          actionType: "backgroundRemoval",
+          fineEdges: true,
+          hints: [ForegroundObject.DOG, ForegroundObject.CAT],
+        },
+      ],
+    });
 
-    expect(transformation.toString()).toStrictEqual([
-      'e_background_removal',
-      'e_background_removal:fineedges_y',
-      'e_background_removal:fineedges_n',
-      'e_background_removal',
-      'e_background_removal:hints_(dog;cat)',
-      'e_background_removal:fineedges_y:hints_(dog;cat)',
-    ].join('/'));
+    expect(transformation.toString()).toStrictEqual(
+      [
+        "e_background_removal",
+        "e_background_removal:fineedges_y",
+        "e_background_removal:fineedges_n",
+        "e_background_removal",
+        "e_background_removal:hints_(dog)",
+        "e_background_removal:hints_(dog;cat)",
+        "e_background_removal:fineedges_y;hints_(dog;cat)",
+      ].join("/")
+    );
   });
 });
