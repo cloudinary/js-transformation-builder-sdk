a4f80cec3dd70667496d2baa395abc053e3780f6
magdakwiecien <97024257+magdakwiecien@users.noreply.github.com>
2024-02-05 12:27:47 +0100

    Make bgColor mapping bulletproof regardless of syntax version (#43)
    * Make bgColor mapping bulletproof regardless of syntax version
    
    * Fix indentation
    
    * Fix semicolon

--- __TESTS__/unit/toJson/backgroundColor.toJson.test.ts
+++ __TESTS__/unit/toJson/backgroundColor.toJson.test.ts
@@ -2,10 +2,19 @@ import {Transformation} from "../../../src";
 import {BackgroundColor} from "../../../src/actions/background/actions/BackgroundColor.js";
 
 describe('BackgroundColor toJson()', () => {
-  it('backgroundColor', () => {
+  it('new BackgroundColor', () => {
     const transformation = new Transformation()
       .addAction(new BackgroundColor('white'))
       .addAction(new BackgroundColor('#ffffff'));
+    expect(transformation.toJson()).toStrictEqual({actions: [
+      { actionType: 'backgroundColor', color: 'white' },
+      { actionType: 'backgroundColor', color: '#ffffff' }
+    ]});
+  });
+  it('.backgroundColor()', () => {
+    const transformation = new Transformation()
+      .backgroundColor('white')
+      .backgroundColor('#ffffff');
     expect(transformation.toJson()).toStrictEqual({actions: [
       { actionType: 'backgroundColor', color: 'white' },
       { actionType: 'backgroundColor', color: '#ffffff' }
